8e88428b99ad04295cd5b3c1b6b29ef21dcd5e8a
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-04-17 15:54:57 -0300

    req 12

--- playground.mongodb.js
+++ playground.mongodb.js
@@ -1,6 +1,14 @@
 use("commerce");
 
+db.produtos.updateMany(
+  { nome: { $ne: 'McChicken'}},
+  { $addToSet: {
+      ingredientes: 'ketchup'
+    }
+  }
+);
+
 db.produtos.find(
-  { nome: { $nin: ['Big Mac', 'McChicken'] } },
-  { _id: 0, nome: 1, curtidas: 1, vendidos: 1 }
+  {},
+  { _id: 0, nome: 1, ingredientes: 1}
 );
