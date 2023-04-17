89a0985dc610528c174c31851cd135e8853f8647
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-04-17 15:15:53 -0300

    req 5

--- playground.mongodb.js
+++ playground.mongodb.js
@@ -3,8 +3,10 @@ use("commerce");
 db.produtos.countDocuments({});
 
 db.produtos.find(
-  { },
-  { _id:0, nome: 1, vendidos: 1 }
-).sort(
-  { vendidos: -1}
-).limit(1);
+  { $or: [
+      { curtidas: 36 },
+      { vendidos: 85 }
+    ]
+  },
+  { _id:0 , nome: 1, vendidos: 1, curtidas: 1 }
+);
