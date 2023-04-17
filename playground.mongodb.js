6978a318911d8e2a530a5e3f060f9d562a96ba75
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-04-17 15:35:52 -0300

    req 9

--- playground.mongodb.js
+++ playground.mongodb.js
@@ -1,10 +1,12 @@
 use("commerce");
 
-db.produtos.deleteMany(
-  {curtidas: { $lt: 50 } }
-);
-
 db.produtos.find(
-  {},
-  { _id: 0, nome: 1}
+  { valoresNutricionais: {
+      $elemMatch: {
+        tipo: 'calorias',
+        quantidade: { $lt: 500 }
+      }
+    }
+  },
+  {_id: 0, nome: 1}
 );
